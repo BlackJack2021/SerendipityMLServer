57309d6d38e9668926626fc4483e7e7ad6aa09b8
blackjack <kurawata414@gmail.com>
2023-06-04 00:58:03 +0900

    同期通信用の設定を追加

--- src/db.py
+++ src/db.py
@@ -2,8 +2,10 @@ from src.db_config import config
 from src.secret import rds_secret
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 
 async_driver = config['async_driver']
+sync_driver = config['sync_driver']
 user = rds_secret['user']
 password = rds_secret['password']
 endpoint = config['endpoint']
@@ -11,6 +13,7 @@ port = config['port']
 database = config['database']
 charset = config['charset']
 
+# 非同期通信用の設定
 ASYNC_DB_URL = f"{async_driver}://{user}:{password}@{endpoint}:{port}/{database}?charset={charset}"
 async_engine = create_async_engine(ASYNC_DB_URL, echo=False)
 async_session = sessionmaker(
@@ -22,3 +25,12 @@ Base = declarative_base()
 async def get_db():
     async with async_session() as session:
         yield session
+        
+# 同期通信用の設定
+SYNC_DB_URL = f"{sync_driver}://{user}:{password}@{endpoint}:{port}/{database}?charset={charset}"
+engine = create_engine(SYNC_DB_URL)
+sync_session = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
