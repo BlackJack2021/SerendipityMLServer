05d432d06c30e52320668f9fc89651b29ddc2355
blackjack <kurawata414@gmail.com>
2023-06-05 14:23:20 +0900

    必要なくなったので学習用 script を削除

--- src/recommend/ml/imf/script/train.py
+++
@@ -1,7 +0,0 @@
-"""学習用のスクリプトです。"""
-from src.recommend.ml.imf.fetcher import fetch_data_for_train
-from src.recommend.ml.imf.trainer import Trainer
-
-if __name__ == "__main__":
-    raw_df = fetch_data_for_train()
-    trainer = Trainer().execute(raw_df=raw_df)
