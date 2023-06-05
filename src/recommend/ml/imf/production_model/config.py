fcae9153a421d19f8c680b6616fb01bc62737b0d
blackjack <kurawata414@gmail.com>
2023-06-05 14:22:43 +0900

    本番用モデルのパラメータ

---
+++ src/recommend/ml/imf/production_model/config.py
@@ -0,0 +1,11 @@
+parameter = {
+    "minimum_num_per_user": None,
+    "valid_ratio": None,
+    "alpha": 1.0,
+    "factors": 20,
+    "n_epochs": 50,
+    "random_state": 1,
+    "k": 10,
+    "regularization": 0.1,
+    "end_date": None,
+}
