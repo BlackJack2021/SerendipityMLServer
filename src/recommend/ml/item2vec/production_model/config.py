fce6f1bbbaeb8f5bbd14b2c81de89c611fcd3a60
blackjack <kurawata414@gmail.com>
2023-06-06 17:06:02 +0900

    production_model が作成できるところまで確認済み

---
+++ src/recommend/ml/item2vec/production_model/config.py
@@ -0,0 +1,12 @@
+parameter = {
+    "minimum_num_per_user": None,
+    "valid_ratio": None,
+    "factors": 100,
+    "n_epochs": 30,
+    "window": 5,
+    "use_skip_gram": 1,
+    "use_hierarchial_softmax": 0,
+    "min_count": 1,
+    "k": 10,
+    "end_date": None,
+}
