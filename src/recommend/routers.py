18948897bc8d797e9b94bb01ce9606a0868d2088
blackjack <kurawata414@gmail.com>
2023-06-06 17:54:46 +0900

    item2vec の推論APIを構築。動作まで確認。

--- src/recommend/routers.py
+++ src/recommend/routers.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
-from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.recommend.ml.imf.production_model.config import parameter
-from src.recommend.ml.imf.predictor import predict_for_user
+from src.recommend.ml.imf.predictor import predict_for_user as imf_predictor
+from src.recommend.ml.item2vec.predictor import predict_for_user as item2vec_predictor
 import src.recommend.schemas as schemas
+from src.db import get_db
 
 router = APIRouter()
 
@@ -16,7 +18,16 @@ def predict_by_imf(user_id: str):
     factors = parameter["factors"]
     n_epochs = parameter["n_epochs"]
     k = parameter["k"]
-    recommended = predict_for_user(
+    recommended = imf_predictor(
         user_id=user_id, factors=factors, n_epochs=n_epochs, k=k
     )
+    return {"edinet_codes": recommended}
+
+
+@router.get(
+    "/ml/recommend/item2vec/predict/{user_id}",
+    response_model=schemas.RecommendedByItem2Vec,
+)
+async def predict_by_item2vec(user_id: str, db: AsyncSession = Depends(get_db)):
+    recommended = await item2vec_predictor(user_id=user_id, db=db)
     return {"edinet_codes": recommended}
