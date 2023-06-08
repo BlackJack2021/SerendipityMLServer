5e5289370d5f4483093bea88f0e3341e35267089
blackjack <kurawata414@gmail.com>
2023-06-08 11:43:01 +0900

    trainer コード修正に伴い、精度の良さそうなハイパラに変更。

--- src/recommend/ml/imf/production_model/config.py
+++ src/recommend/ml/imf/production_model/config.py
@@ -2,7 +2,7 @@ parameter = {
     "minimum_num_per_user": None,
     "valid_ratio": None,
     "alpha": 1.0,
-    "factors": 20,
+    "factors": 100,
     "n_epochs": 50,
     "random_state": 1,
     "k": 10,
