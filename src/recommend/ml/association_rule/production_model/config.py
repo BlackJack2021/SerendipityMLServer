d5047308a851410a1a4013564a48863e17f06dbb
blackjack <kurawata414@gmail.com>
2023-06-08 11:45:15 +0900

    production 用のモデルを学習するスクリプトを用意

---
+++ src/recommend/ml/association_rule/production_model/config.py
@@ -0,0 +1,9 @@
+parameter = {
+    "minimum_num_per_user": 12,
+    "valid_ratio": None,
+    "min_support": 0.01,
+    "metric": "lift",
+    "min_threshold": 1,
+    "k": 10,
+    "end_date": None,
+}
